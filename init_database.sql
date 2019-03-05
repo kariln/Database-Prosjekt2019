d7ba6eae8db4f21a0b7d658b6b0b8d1969f6ecfa
Martin Gr <martin.groenvold@online.no>
2019-03-05 11:31:53 +0100

    Lagt til relasjonsdatabasemodell i readmefilen

---
+++ init_database.sql
@@ -0,0 +1,21 @@
+-- Create Diary/user (dagbok bruker)
+
+-- Create Note (notat)
+CREATE TABLE note (
+    id INT NOT NULL,
+    purpose TEXT,
+    experience INT, -- 1-10 points¨
+    other TEXT
+);
+
+-- Create Exercise (Treningsøkt)
+CREATE TABLE workout (
+
+);
+
+-- Create Result (Resultat)
+CREATE TABLE result (
+
+);
+
+--
