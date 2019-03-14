df2f19a7e1825fba48376293c5a0a4ace2ba8360
sglzf <36081915+sglzf@users.noreply.github.com>
2019-03-14 12:57:41 +0100

    øvelse m apparat

--- Databaseprosjektv19/src/modeller/Øvelse_apparat.java
+++ Databaseprosjektv19/src/modeller/Øvelse_apparat.java
@@ -1,5 +1,37 @@
 package modeller;
 
-public class Øvelse_apparat {
+import java.sql.Connection;
+
+public class Øvelse_apparat extends Øvelse implements ActiveDomainObject{
+
+	private int kg;
+	private int antall_sett;
+	private Apparat apparat;
+	
+	public Øvelse_apparat(int id, String navn, int kg, int sett) {
+		super(id, navn);
+		this.kg = kg;
+		this.antall_sett = sett;
+	}
+	
+	
+	
+	@Override
+	public void initialize(Connection conn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void refresh(Connection conn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save(Connection conn) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
