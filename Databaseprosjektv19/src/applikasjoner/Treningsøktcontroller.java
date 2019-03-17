08d56115badae34613c6ed9bbafe7fbdef8a93a7
sglzf <36081915+sglzf@users.noreply.github.com>
2019-03-17 18:01:27 +0100

    Bare lagt inn kommentarer om funksjonalitet

--- Databaseprosjektv19/src/applikasjoner/Treningsøktcontroller.java
+++ Databaseprosjektv19/src/applikasjoner/Treningsøktcontroller.java
@@ -3,5 +3,12 @@ package applikasjoner;
 public class Treningsøktcontroller {
 	
 	
+	
+	//legge til treningsøkt med tilhørende data
+	
+	
+	
+	// få opp n sist gjennomførte treningsøkter med notater, der n spesifiseres av bruker
+	
 
 }
